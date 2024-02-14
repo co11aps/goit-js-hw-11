4ecc1b1446523937c91f6f6c576c3aae1eff482b
co11aps <143988142+co11aps@users.noreply.github.com>
2024-02-14 15:32:17 +0800

    111

--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
 console.log('Main');
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+// Описаний у документації
+import SimpleLightbox from 'simplelightbox';
+// Додатковий імпорт стилів
+import 'simplelightbox/dist/simple-lightbox.min.css';
